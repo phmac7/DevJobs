9caca5170fbbd74e6633c932686c572c214f2d3d
Pedro Machado <pedro.machado@valtech.com>
2022-11-08 23:52:24 -0300

    chore: add next-themes for darkMode theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,25 @@
+'use client';
 import '@/styles/globals.css';
+import { ThemeProvider } from 'next-themes';
 
-export default async function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <ThemeProvider attribute="class">
+        <body
+          className="bg-gray-50
+        dark:bg-gray-900
+        text-gray-900
+        dark:text-gray-50
+        transition-colors"
+        >
+          {children}
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import ThemeChanger from '@/components/ThemeChanger';
+'use client';
+
+import ThemeToggle from '@/components/ThemeToggle/ThemeToggle';
 
 export default function Page() {
   return (
-    <h1>Hello world!</h1>
+    <div className="max-w-screen-md mx-auto">
+      <header className="flex justify-between items-center p-8">
+        <h1 className="text-xl">Hello world</h1>
+        <ThemeToggle />
+      </header>
+    </div>
   );
 }
