a1c26514d6a5e101694875366f164909fae92989
Pedro Machado <pedro.machado@valtech.com>
2022-11-11 00:14:42 -0300

    feat: Added the header Component

---
+++ components/organisms/Header/index.tsx
@@ -0,0 +1,26 @@
+import { ThemeToggle } from '@/components/atoms';
+import { SearchInput } from '@/components/molecules';
+import Image from 'next/image';
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={`${styles.header} w-full h-36`}>
+      <div className="flex pt-8 px-6 gap-8 flex-col">
+        <div className="flex justify-between items-center">
+          <Image
+            src={'/assets/desktop/logo.svg'}
+            alt={'DevJobs logo: just the word in white color'}
+            width={115}
+            height={32}
+          />
+          <ThemeToggle />
+        </div>
+        <SearchInput />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
