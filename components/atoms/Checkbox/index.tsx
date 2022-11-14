da76add74e42e35c83a07f49827fc1eec532c57f
Pedro Machado <pedro.machado@valtech.com>
2022-11-14 14:38:10 -0300

    feat: add handling checkbox states

--- components/atoms/Checkbox/index.tsx
+++ components/atoms/Checkbox/index.tsx
@@ -4,15 +4,22 @@ import React from 'react';
 
 interface CheckBoxProps {
   label: string;
+  fullTime: boolean;
+  setFullTime: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Checkbox: React.FC<CheckBoxProps> = ({ label }) => {
+const Checkbox: React.FC<CheckBoxProps> = ({
+  label,
+  fullTime,
+  setFullTime,
+}) => {
   return (
     <div className="flex items-center mb-4 tablet:h-full tablet:mb-0">
       <input
         id="default-checkbox"
         type="checkbox"
-        value=""
+        checked={fullTime}
+        onChange={(e) => setFullTime(e.target.checked)}
         className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 "
       />
       <label
