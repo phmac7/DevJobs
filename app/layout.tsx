d71e0f39c2f6376e86abb6fd5661e6840c9812c2
Pedro Machado <phvieiras@hotmail.com>
2023-01-21 14:17:15 -0300

    feat: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/organisms/Header';
 import '@/styles/globals.css';
 import { Kumbh_Sans } from '@next/font/google';
 import { ThemeProvider } from 'next-themes';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 const kumbh = Kumbh_Sans({ subsets: ['latin'] });
 
 export const revalidate = 60;
@@ -12,19 +13,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={kumbh.className}>
-      <ThemeProvider attribute="class">
-        <body
-          className="bg-light-gray
+    <>
+      <html lang="en" className={kumbh.className}>
+        <GoogleAnalytics trackPageViews />
+        <ThemeProvider attribute="class">
+          <body
+            className="bg-light-gray
         dark:bg-midnight
         text-gray-900
         dark:text-gray
         transition-colors"
-        >
-          <Header />
-          <main className="mx-6">{children}</main>
-        </body>
-      </ThemeProvider>
-    </html>
+          >
+            <Header />
+            <main className="mx-6">{children}</main>
+          </body>
+        </ThemeProvider>
+      </html>
+    </>
   );
 }
